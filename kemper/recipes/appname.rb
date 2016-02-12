a0cf68d08d25d8f7ae8630aa93f3cdf7e8606f14
Thinh Le <tle@kemper.com>
2016-02-12 13:53:45 -0500

    updated index for app name

--- kemper/recipes/appname.rb
+++ kemper/recipes/appname.rb
@@ -1,4 +1,4 @@
-if my_app = node["opsworks"]["stack"]["applications"][1]["slug_name"]
+if my_app = node["opsworks"]["stack"]["applications"][0]["slug_name"]
 	template "/etc/apache2/sites-enabled/kemper.txt" do
 	source "kemper.erb"
 	variables :appname => my_app
